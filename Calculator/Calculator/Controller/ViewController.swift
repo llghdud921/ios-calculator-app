77f0fbe95361ced6b05d8d077aae601dcaf78c39
llghdud921 <llghdud921@naver.com>
2021-11-18 01:03:46 +0900

    refactor: 숫자 버튼 클릭시 기능 재구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -23,65 +23,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var operatorSettingLabel: UILabel!
     
     @IBAction func clickNumber(_ sender: NumberButton) {
-        let numberFormatter = NumberFormatter()
         
-        numberFormatter.numberStyle = .decimal
-        numberFormatter.maximumIntegerDigits = 20
-        
-        var digit = 0
-        
-        if numberCompositionLabel.text == "0" {
-            numberCompositionLabel.text = sender.number
-        } else {
-            // String
-            guard var numberStringOfLabel = numberCompositionLabel.text else {
-                return
-            }
-            
-            guard let clickedNumber = sender.number, let clickedValue = Int(clickedNumber) else {
-                return
-            }
-            
-            if numberStringOfLabel.contains(".") {
-                if numberStringOfLabel.hasSuffix(".") {
-                    numberStringOfLabel.removeLast()
-                    digit = -1
-                } else {
-                    let splitedNumber = numberStringOfLabel.split(separator: ".")
-                    digit = ( splitedNumber[1].count * -1 ) - 1
-                }
-            }
-            
-            // String -> Number
-            guard let numberValue = numberFormatter.number(from: numberStringOfLabel) else {
-                return
-            }
-            
-            var compositedValue: Double = Double.zero
-            
-            if digit < 0 {
-                compositedValue = Double(truncating: numberValue) + Double(clickedValue) * (10 ** digit)
-            } else {
-                compositedValue = Double(truncating: numberValue) * 10 + Double(clickedValue)
-            }
-            
-            numberCompositionLabel.text = numberFormatter.string(from: NSNumber(value: compositedValue))
-        }
-    }
-    
-    @IBAction func clickPointButton(_ sender: NumberButton) {
-        let numberFormatter = NumberFormatter()
-        
-        numberFormatter.numberStyle = .decimal
-        numberFormatter.maximumIntegerDigits = 20
-        
-        //String
-        guard let numberStringOfLabel = numberCompositionLabel.text else {
+        guard let numberOfLabel = numberCompositionLabel.text else {
             return
         }
         
-        // String
-        numberCompositionLabel.text = numberStringOfLabel + "."
+        guard let numberOfButton = sender.number else {
+            return
+        }
+        
+        if numberOfLabel == "0" {
+            numberCompositionLabel.text = sender.number
+        } else {
+            numberCompositionLabel.text = numberOfLabel + numberOfButton
+        }
     }
 }
 
