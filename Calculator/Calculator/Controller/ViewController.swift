d5b3052e8a2d344a38b587f6896e4e56c3c33832
llghdud921 <llghdud921@naver.com>
2021-11-17 16:05:17 +0900

    feat: custom operator버튼 만들고 아웃렛 변수에 해당 버튼 담기

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     }
     
     @IBOutlet var numberButtons: [NumberButton]!
-
+    @IBOutlet var operatorButtons: [OperatorButton]!
+    
 }
 
---
+++ Calculator/Calculator/View/OperatorButton.swift
@@ -0,0 +1,12 @@
+//
+//  OperatorButton.swift
+//  Calculator
+//
+//  Created by 이호영 on 2021/11/17.
+//
+
+import UIKit
+
+class OperatorButton: UIButton {
+    var `operator`: Character?
+}
